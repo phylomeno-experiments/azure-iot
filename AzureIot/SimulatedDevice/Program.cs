986ca12f659ff93d51bc309875b9f2d5e62b1989
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-18 06:09:59 +0200

    implement message sending example

--- AzureIot/SimulatedDevice/Program.cs
+++ AzureIot/SimulatedDevice/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
 
 namespace SimulatedDevice
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("IoT Hub Simulated Device");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please pass connectionString as first command line argument");
+                return;
+            }
+
+            SendMessagesToHubAsync(args[1]);
+            Console.ReadLine();
+        }
+
+        private static async void SendMessagesToHubAsync(string connectionString)
+        {
+            var deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+            var rand = new Random();
+            while (true)
+            {
+                var messageJson = JsonConvert.SerializeObject(new
+                {
+                    value = 45 + rand.NextDouble() * 10
+                });
+
+                var message = new Message(Encoding.ASCII.GetBytes(messageJson));
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} - Sending message {1}", DateTime.Now, messageJson);
+
+                await Task.Delay(1000);
+            }
         }
     }
 }
