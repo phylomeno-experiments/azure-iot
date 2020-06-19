cba7eb1a747cc5750ebe613c77469c981b5f396f
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-19 06:30:01 +0200

    implement exit handler token

--- AzureIot/EchoBackend/Program.cs
+++ AzureIot/EchoBackend/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace EchoBackend
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("IoT Hub Echo Backend");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please pass eventHubEndpoiknt as first command line argument");
+                return;
+            }
+            
+            var cancellationSource = new CancellationTokenSource();
+
+            var cancellationToken = CreateExitHandlerToken(cancellationSource);
+            await ReceiveMessagesAsync(cancellationToken);
+        }
+
+        private static CancellationToken CreateExitHandlerToken(CancellationTokenSource cancellationSource)
+        {
+            void CancelKeyPressHandler(object sender, ConsoleCancelEventArgs eventArgs)
+            {
+                eventArgs.Cancel = true;
+                cancellationSource.Cancel();
+                Console.WriteLine("Exiting...");
+
+                Console.CancelKeyPress -= CancelKeyPressHandler;
+            }
+
+            Console.CancelKeyPress += CancelKeyPressHandler;
+
+            var cancellationToken = cancellationSource.Token;
+            return cancellationToken;
+        }
+
+        private static async Task ReceiveMessagesAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
     }
 }
