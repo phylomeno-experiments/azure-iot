b43596ed970686ed3ece6d61abff8dc04359ad9a
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-27 15:05:39 +0200

    provisioning client construction

--- AzureIot/AutoProvisionedDevice/Program.cs
+++ AzureIot/AutoProvisionedDevice/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
 
 namespace AutoProvisionedDevice
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            X509Certificate2 certificate;
+            using (var security = new SecurityProviderX509Certificate(certificate))
+            using (var transport = new ProvisioningTransportHandlerMqtt())
+            {
+                var provisioningClient = ProvisioningDeviceClient.Create("global.azure-devices-provisioning.net",
+                    "idscope", security, transport);
+            }
         }
     }
 }
